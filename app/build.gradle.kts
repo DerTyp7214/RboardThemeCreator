49af2bf4761745b358e7e2417aebd92dba4c1709
PahaAkos <paha.akos@gmail.com>
2024-02-22 14:32:04 +0100

    Update Dependencies
    New dependencies changes:
    • Update Browser to 1.8.0-rc01
    • Update Webkit to 1.11.0-alpha02
    
    Others Changes:
    • Update versionCode to 125002 (1.2.5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "de.dertyp7214.rboardthemecreator"
         minSdk = 23
         targetSdk = 34
-        versionCode = 125001
+        versionCode = 125002
         versionName = "1.2.5"
 
         vectorDrawables.useSupportLibrary = true
